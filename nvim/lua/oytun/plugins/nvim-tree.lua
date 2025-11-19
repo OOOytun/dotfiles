a375b8c687ebc4ca20f06e1667940be333f148bd
Oytun <oytuntutuncu86@gmail.com>
2025-11-19 13:48:16 +0100

    revise nvim tree

--- nvim/lua/oytun/plugins/nvim-tree.lua
+++ nvim/lua/oytun/plugins/nvim-tree.lua
@@ -48,9 +48,10 @@ return {
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>et", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+    keymap.set("n", "<leader>es", "<cmd>NvimTreeFindFile<CR>", { desc = "Show file explorer and focus on current file" }) -- show file explorer and focus on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Open and focus on current file" }) -- open and focus on current file
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end
 }
